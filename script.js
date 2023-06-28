79bdfd11c085df83950bda99ad955ddd25c285a2
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-06-28 16:10:39 -0700

    Completed Project Cards and Contact Form

--- script.js
+++ script.js
@@ -15,9 +15,46 @@ function onTypingPaused() {
   if (typed.loop == true) {
     typed.loop = false
   } else {
-  typed.loop = true
-  typed.reset()
+    typed.loop = true
+    typed.reset()
   }
 }
 
+
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  var formEmail = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+        console.log("invalid");
+      } else {
+        formEmail.name = document.getElementById('name').value
+        formEmail.email = document.getElementById('email').value
+        formEmail.message = document.getElementById('message').value
+
+        window.open(`mailto:kicans@gmail.com
+        ?subject=Portfolio Contact Form
+        &body=Name: ${formEmail.name}
+              Message: ${formEmail.message}
+              Email: ${formEmail.email}`);
+
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
 
